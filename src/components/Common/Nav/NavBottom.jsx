df69546b5946841e6a54f4753dd44daedb9d478f
dongsu.han <kku304@naver.com>
2023-10-22 15:14:29 +0900

    ✨feature/27-NavCss

--- src/components/Common/Nav/NavBottom.jsx
+++ src/components/Common/Nav/NavBottom.jsx
@@ -1,63 +1,50 @@
-import React from 'react';
-import styled from 'styled-components';
-// import './NavBottom.css';
+import React, { useState } from 'react';
 import home from '../../../assets/icons/icon-home.svg';
 import calender from '../../../assets/icons/icon-calendar.svg';
-import chat from '../../../assets/icons/icon-chat.svg';
 import community from '../../../assets/icons/icon-community.svg';
 import profile from '../../../assets/icons/icon-my.svg';
-
-const StyledNavIcons = styled.img`
-  width: 28px;
-  height: 28px;
-`;
-const Nav = styled.nav`
-  overflow: hidden;
-  background-color: #141414;
-  color: white;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 70px;
-  border-top: 0.5px solid #fff;
-`;
-
-const NavDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  float: left;
-  width: 20%;
-  height: 45px;
-  padding-top: 30px;
-  gap: 5px;
-`;
+import home_fill from '../../../assets/icons/icon-home-fill.svg';
+import calender_fill from '../../../assets/icons/icon-calendar-fill.svg';
+import community_fill from '../../../assets/icons/icon-community-fill.svg';
+import profile_fill from '../../../assets/icons/icon-my-fill.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Nav, NavDiv, StyledNavIcons, NavText } from './NavStyles';
 
 export default function NavBottom() {
+  const [activeTab, setActiveTab] = useState('home');
+
+  const handleClick = (tab) => {
+    setActiveTab(tab);
+  };
   return (
     <Nav>
-      <NavDiv>
-        <StyledNavIcons src={home} alt='home icon' />홈
-      </NavDiv>
-      <NavDiv>
-        <StyledNavIcons src={calender} alt='home icon' />
-        캘린더
-      </NavDiv>
-      <NavDiv>
-        <StyledNavIcons src={chat} alt='home icon' />
-        채팅
-      </NavDiv>
-      <NavDiv>
-        <StyledNavIcons src={community} alt='home icon' />
-        커뮤니티
-      </NavDiv>
-      <NavDiv>
-        <StyledNavIcons src={profile} alt='home icon' />
-        마이페이지
-      </NavDiv>
+      <BrowserRouter>
+        <NavDiv onClick={() => handleClick('home')}>
+          <StyledNavIcons src={activeTab === 'home' ? home_fill : home} alt='home icon' />
+          <NavText isActive={activeTab === 'home'}>홈</NavText>
+        </NavDiv>
+        <NavDiv onClick={() => handleClick('calender')}>
+          <StyledNavIcons
+            src={activeTab === 'calender' ? calender_fill : calender}
+            alt='calender icon'
+          />
+          <NavText isActive={activeTab === 'calender'}>캘린더</NavText>
+        </NavDiv>
+        <NavDiv onClick={() => handleClick('community')}>
+          <StyledNavIcons
+            src={activeTab === 'community' ? community_fill : community}
+            alt='community icon'
+          />
+          <NavText isActive={activeTab === 'community'}>커뮤니티</NavText>
+        </NavDiv>
+        <NavDiv onClick={() => handleClick('profile')}>
+          <StyledNavIcons
+            src={activeTab === 'profile' ? profile_fill : profile}
+            alt='profile icon'
+          />
+          <NavText isActive={activeTab === 'profile'}>마이페이지</NavText>
+        </NavDiv>
+      </BrowserRouter>
     </Nav>
   );
 }
