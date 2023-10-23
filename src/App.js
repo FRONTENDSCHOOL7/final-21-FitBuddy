67c944275105835138f5cbc5bc89f5e91fbfe52c
GoodSilver9 <hool784@naver.com>
2023-10-23 15:36:40 +0900

    ✨ : 캘린더 컴포넌트 구현 작업

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import NavTopBack from './components/Common/Nav/NavTopBack';
 import NavTopBasic from './components/Common/Nav/NavTopBasic';
 import NavTopDetails from './components/Common/Nav/NavTopDetails';
 import PlaceHolder from './components/Common/Placeholder/PlaceHolder';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import Onboard from './pages/onBoard/OnBoardingPage';
 import Home from './pages/mainhome/Home';
@@ -13,21 +12,15 @@ import Community from './pages/community/Community';
 import Mypage from './pages/mypage/Mypage';
 import JoinPage from './pages/join/JoinPage';
 import ErrorPage from './pages/ErrorPage';
+import CalendarContent from './components/Calendar/CalendarContent';
+import CalendarComponent from './components/Calendar/CalendarComponent';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/onboard' element={<Onboard />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/calender' element={<Calender />} />
-        <Route path='/community' element={<Community />} />
-        <Route path='/mypage' element={<Mypage />} />
-        <Route path='/join' element={<JoinPage />} />
-        <Route path='/error' element={<ErrorPage />} />
-      </Routes>
-      <NavBottom />
-    </Router>
+    <>
+      <CalendarContent emoji='💪' title='헬스 배우고 싶은 분' time='오후:6:00 ~ 오후 7:00' />
+      <CalendarComponent />
+    </>
   );
 }
 export default App;
