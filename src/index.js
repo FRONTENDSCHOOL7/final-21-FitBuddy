7f8e3a37e4b0595ce4cd478478ca551e62c96679
박규경 <64006112+ggpark1218@users.noreply.github.com>
2023-10-24 10:16:21 +0900

    ✨ Feature: routes 폴더 생성 및 Router 설정

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import GlobalStyle from './styles/GlobalStyle';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(
-  <>
-    <GlobalStyle />
-    <App />
-  </>,
-);
+root.render(<App />);
