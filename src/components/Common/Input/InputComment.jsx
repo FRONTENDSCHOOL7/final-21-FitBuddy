08e7c00bddc9b3af46bb99d18f1533763907e3d4
GoodSilver9 <hool784@naver.com>
2023-11-06 14:49:32 +0900

    겹치는 파일 스타일파일 이전

--- src/components/Common/Input/InputComment.jsx
+++ src/components/Common/Input/InputComment.jsx
@@ -5,38 +5,6 @@ import { useRecoilState } from 'recoil';
 import { StyledButton, StyledDiv, StyledInput } from './style/StyledInputComment';
 import PlaceHolder from '../Placeholder/PlaceHolder';
 
-const StyledInput = styled.input`
-  background: none;
-  border: none;
-  color: #fff;
-  margin-left: 15px;
-  padding-right: 120px;
-  width: 300px;
-
-  &:focus {
-    outline: none;
-  }
-`;
-const StyledDiv = styled.div`
-  display: flex;
-  width: 100%;
-  background-color: #141414;
-  padding: 10px;
-  justify-content: center;
-  bottom: 80px;
-  position: fixed;
-  min-height: 60px;
-  border-radius: 10px;
-`;
-const StyledButton = styled.button`
-  background-color: #141414;
-  border: none;
-  color: #fff;
-  &:hover {
-    color: var(--color-primary);
-  }
-`;
-
 export default function InputComment(props) {
   const [userToken, setUserToken] = useRecoilState(userTokenAtom);
   return (
--- src/components/Common/Placeholder/PlaceHolder.jsx
+++ src/components/Common/Placeholder/PlaceHolder.jsx
@@ -1,57 +1,5 @@
 import React from 'react';
 import { StyledPlaceHolder } from './StyledPlaceHolder';
-import styled from 'styled-components';
-import icon from '../../../assets/placeholder/Placeholder-icon.svg';
-
-const StyledPlaceHolder = styled.img.attrs((props) => ({
-  src: props.src || icon,
-}))`
-  width: ${(props) =>
-    props.type === 'Ractangle'
-      ? '360px'
-      : props.type === 'Ractangle2'
-      ? '316px'
-      : props.type === 'Photo'
-      ? '414px'
-      : props.type === 'Comment'
-      ? '54px'
-      : props.type === 'Person'
-      ? '42px'
-      : props.type === 'JoinMember'
-      ? '60px'
-      : '100px'};
-  height: ${(props) =>
-    props.type === 'Ractangle'
-      ? '204px'
-      : props.type === 'Ractangle2'
-      ? '204px'
-      : props.type === 'Photo'
-      ? '228px'
-      : props.type === 'Comment'
-      ? '54px'
-      : props.type === 'Person'
-      ? '42px'
-      : props.type === 'JoinMember'
-      ? '60px'
-      : '100px'};
-  border-radius: ${(props) =>
-    props.type === 'Person'
-      ? '20px'
-      : props.type === 'Comment'
-      ? '50px'
-      : props.type === 'JoinMember'
-      ? '20px'
-      : props.type === 'Photo'
-      ? '0px'
-      : '10px'};
-  background-color: #f2f2f2;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-image: ${(props) => (props.src ? 'none' : `url(${icon})`)};
-  background-repeat: no-repeat;
-  background-position: center center;
-`;
 
 function PlaceHolder(props) {
   return <StyledPlaceHolder type={props.type} src={props.src} />;
